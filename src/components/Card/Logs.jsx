f5858a72350d693f159e3e1cccfe64013f26ae18
Anderson Lopez <lopezanderson069@gmail.com>
2023-03-19 18:57:21 -0500

    filter task for value

--- src/components/Card/Logs.jsx
+++ src/components/Card/Logs.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsTrash } from 'react-icons/bs';
 import { AiFillEdit } from 'react-icons/ai'
 import { BiSave } from "react-icons/bi";
+import { CgMoreVertical } from "react-icons/cg";
 import axios from 'axios';
 import ModalReusable from '../Modal/ModalReusable';
 
@@ -17,6 +18,7 @@ const Logs = ({ todo, updateTodos }) => {
   const [error, setError] = useState(false);
   //Manejador de éxito
   const [success, setSuccess] = useState(false);
+  const [seeMore, setSeeMore] = useState(false)
   //Extraccion de valores
   const { title, descripcion } = task;
 
@@ -58,6 +60,16 @@ const Logs = ({ todo, updateTodos }) => {
     setSuccess(false)
   }
 
+  const handleSeeMore = (id) => {
+    setCurrentId(id)
+    setSeeMore(true);
+  }
+
+  const handleSeeLess = () => {
+    setCurrentId("")
+    setSeeMore(false)
+  }
+
   const handleOpenModal = (id) => {
     setCurrentId(id)
     setStateModal(!stateModal)
@@ -74,7 +86,7 @@ const Logs = ({ todo, updateTodos }) => {
   }
 
   return (
-    <div className={`scrollbar scrollbar-thumb-gray-600 scrollbar-track-gray-400 rounded-2xl w-4/5 h-3/4 bg-gray-600/30 flex flex-col justify-start overflow-y-scroll items-center p-6`}>
+    <div className={`text-gray-400 scrollbar justify-center w-full h-full bg-gray-600/30 flex flex-wrap items-center p-6`}>
       {
         todo.map((todo, index) => (
           todo.id === currentId
@@ -93,21 +105,29 @@ const Logs = ({ todo, updateTodos }) => {
       {todo
         ?
         todo.map((todo, index) => (
-          <div key={index} className={`flex flex-col rounded-2xl w-full bg-slate-700 my-7 p-5`}>
-            <div className={`flex text-gray-400 `}>
-              <h1 className={`text-xl my-1`}><strong>Titulo:</strong></h1>
-              <h1 className={`text-2xl mx-1 text-gray-300 px-2 `}><>{todo.title}</></h1>
-              <h1 className={`text-xl my-1`}><strong>Nº de Tarea:</strong></h1>
-              <h1 className={`text-2xl mx-1 text-gray-300 px-2 `}><>{todo.id}</></h1>
+          <div key={index} className={`flex flex-col rounded-2xl w-[25rem] min-h-[32rem] bg-slate-700 m-7 p-5`}>
+            <div className={`flex justify-around flex-col h-[8rem]`}>
+              <div className={`flex  text-gray-400 `}>
+                <h1 className={`text-xl my-1`}><strong>Titulo:</strong></h1>
+                <h1 className={`text-xl mx-1  text-gray-300 px-2 `}><>{todo.title}</></h1>
+              </div>
+              <div className={`flex  text-gray-400 `}>
+                <h1 className={`text-xl my-1`}><strong>Nº de Tarea:</strong></h1>
+                <h1 className={`text-xl mx-1 text-gray-300 px-2 `}><>{todo.id}</></h1>
+              </div>
             </div>
-            <div className={`flex flex-col text-gray-400 `}>
-              <h1 className={`text-1xl my-1`}><strong>Dsescripcion:</strong></h1>
+            <div className={`flex flex-col text-gray-400 overflow-hidden duration-300 h-[14rem] my-[1rem] `}>
+              <h1 className={`text-xl my-1`}><strong>Descripcion:</strong></h1>
               <p className={`text-justify`}>{todo.description}</p>
             </div>
-            <div className={`flex w-full justify-center my-3`}>
+            <div className={`flex w-full h-[4rem]  justify-center items-center mt-3`}>
               <div onClick={() => handleOpenModal(todo.id)} className={`flex justify-between items-center h-8 p-1 bg-gray-800 mx-3 rounded-md hover:bg-slate-200 hover:text-black duration-300 cursor-pointer`}>
                 <AiFillEdit className={`mx-1`} />
                 <h2 className={`mx-1`}>Editar</h2>
+              </div>
+              <div onClick={todo.id === currentId ? () => handleSeeLess() : () => handleSeeMore(todo.id)} className={`flex justify-between items-center h-8 p-1 bg-gray-800 mx-3 rounded-md hover:bg-slate-200 hover:text-black duration-300 cursor-pointer`}>
+                <CgMoreVertical className={`mx-1`} />
+                <h2 className={`mx-1`}>Ver Mas</h2>
               </div>
               <div onClick={() => handleDeleteTask(todo.id)} className={`flex justify-between items-center h-8 p-1 bg-gray-800 mx-3 rounded-md hover:bg-slate-200 hover:text-black duration-300 cursor-pointer`}>
                 <BsTrash className={`mx-1`} />
@@ -117,7 +137,10 @@ const Logs = ({ todo, updateTodos }) => {
 
           </div>
         ))
-        : <h1>no hay registros</h1>
+        : 
+        <div className={`h-full w-full`}>
+          <h1>no hay registros</h1>
+        </div>
       }
 
     </div>
